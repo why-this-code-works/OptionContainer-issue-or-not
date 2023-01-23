f8465147022bc6a92559b3edbde916cceed46367
Limon Monte <limonte@users.noreply.github.com>
2023-01-23 12:26:05 +0200

    chore: proof

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-import { Button, Logo, Text } from '@coachhubio/nova';
+import { Select, Option } from '@coachhubio/nova-select';
+import { OptionContainer } from '@coachhubio/nova-dropdown-select';
+import styled from 'styled-components';
+
+const MySelect = styled(Select)`
+  ${OptionContainer} {
+    background-color: red;
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [val, setVal] = useState('2');
 
   return (
     <div className="App">
-      <Logo />
-      <Text type="heading" size="l">
-        Nova Design System
-      </Text>
-      <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+      <MySelect value={val} onChange={setVal}>
+        <Option value="1" title="1" />
+        <Option value="2" title="2" />
+      </MySelect>
       <p>
         Edit <code>src/App.tsx</code> to make changes.
       </p>
